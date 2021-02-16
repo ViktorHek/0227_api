23ce764940e91a9afd1ee57ef806828ca8dd8e20
viktor <viktorkarlsson125@gmail.com>
2021-02-16 20:17:17 +0100

    adding and configure rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,25 @@ module Api0227
   class Application < Rails::Application
     config.load_defaults 6.0
     config.api_only = true
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.request_specs false
+    end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: %i[get post put delete],
+          expose: %w[access-token expiry token-type uid client],
+          max_age: 0
+      end
+    end
   end
 end
