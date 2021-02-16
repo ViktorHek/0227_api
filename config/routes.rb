36af57c26f19f02123354ea2a8b35bd24a4ce8a6
viktor <viktorkarlsson125@gmail.com>
2021-02-16 20:47:23 +0100

    adding index spec. product controller and product route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    resources :products, only: [:index]
+  end
 end
---
+++ spec/requests/user_can_see_list_of_pruducts_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe 'GET/api/products' do
+  describe 'successfully get list of products' do
+    before do
+      get '/api/products'
+    end
+
+    it 'is expected to return a 200 response' do
+      expect(response).to have_http_status 200
+    end
+  
+  end
+end
